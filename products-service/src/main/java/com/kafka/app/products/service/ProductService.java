6ce9dd2e5db177d05fd4d04b9dcfc1793a729dc1
paul566576 <slimm566576@gmail.com>
2025-02-01 21:07:50 +0100

    ProductService has been created

---
+++ products-service/src/main/java/com/kafka/app/products/service/ProductService.java
@@ -0,0 +1,13 @@
+package com.kafka.app.products.service;
+
+import com.kafka.app.core.dto.Product;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ProductService {
+    List<Product> findAll();
+    Product reserve(Product desiredProduct, UUID orderId);
+    void cancelReservation(Product productToCancel, UUID orderId);
+    Product save(Product product);
+}
